de23423af775350709c7778ab075cf7eba803640
mariana.vns <mariana.vns@gmail.com>
2020-10-21 13:22:47 -0200

    Adds app.js file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-//
+// Importando os módulos
+const express = require('express')
+const app = express()
+
+// Usando módulos importados
+app.use(express.json())
+
+// Importando as rotas
+const index = require('./routes/index')
+const routes = require('./routes/seriesRoutes')
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Request-with, Content-Type, Accept'
+    )
+    next()
+})
+
+// Definindo as rotas
+app.use('/', index)
+app.use('/series', routes)
+
+// Disponibilizando o arquivo
+module.exports = app
