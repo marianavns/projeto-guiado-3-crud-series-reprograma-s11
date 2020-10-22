b7fca0a7107e0c207d82dc83d05eb415f4ceda31
marianavns <mariana.vns@gmail.com>
2020-10-22 11:40:15 -0300

    Fix bug in seriesController that deleted registered ids when the user entered a non-existent id.

--- src/controllers/seriesController.js
+++ src/controllers/seriesController.js
@@ -1,17 +1,15 @@
 const fs = require('fs')
 const series = require( '../models/series.json' )
 
-
-const IDsPreenchidas = series.map(element => (element.id))
-const writeJSON = (frase) => {
+const filledIDs = series.map(element => (element.id))
+const writeJSON = (phrase) => {
     fs.writeFile('./src/models/series.json', JSON.stringify(series), 'utf8', function(err){
         if (err) {
             return res.status(424).send({message: err})
         }
-    console.log(`${frase}`)
+    console.log(`${phrase}`)
         })
     }
-
 
 const getAllSeries = (req, res) => {
     res.status(200).send(series)
@@ -27,8 +25,8 @@ const postSerie = (req, res) => {
     const {id, name, genre, synopsis, liked, seasons} = req.body
     console.log(id)
 
-    if (IDsPreenchidas.indexOf(id) != -1) {
-        res.status(201).send(`Não foi possível adicionar a série ${name} ao arquivo pois a ID escolhida (${id}) já está preenchida. Por favor, escolha números diferentes de ${IDsPreenchidas} para a nova série a ser cadastrada.`)
+    if (filledIDs.indexOf(id) != -1) {
+        res.status(201).send(`Não foi possível adicionar a série ${name} ao arquivo pois a ID escolhida (${id}) já está preenchida. Por favor, escolha números diferentes de ${filledIDs} para a nova série a ser cadastrada.`)
     } else {
         series.push({id, name, genre, synopsis, liked, seasons})
             function compare(a,b) {
@@ -44,17 +42,20 @@ const postSerie = (req, res) => {
     }
 }
 
-
 // const putSerie = (req, res) => {
 // }
 
 const deleteSerie = (req, res) => {
-    const id = req.params.id
-    const serieToDelete = series.find(element => element.id == id)
-    const index = series.indexOf(serieToDelete)
-    series.splice(index, 1)
-    writeJSON(`A série com o id ${id} foi deletada!`) 
-    res.status(201).send(series)
+    const id = parseInt(req.params.id)
+    if (filledIDs.indexOf(id) == -1) {
+        res.status(201).send(`A série com o ID solicitado para a deleção (id: ${id}) não existe no banco de dados. Os IDs passíveis de deleção são ${filledIDs}.`)
+        } else {
+        const serieToDelete = series.find(element => element.id == id)
+        const index = series.indexOf(serieToDelete)
+            series.splice(index, 1)
+            writeJSON(`A série com o id ${id} foi deletada!`) 
+        res.status(201).send(series)
+            }
 }
 
 // const patchSerie = (req, res) => {
