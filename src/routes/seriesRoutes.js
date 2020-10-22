40f717b12d51a6abe0372d4e4fe3084a6ac87aaa
marianavns <mariana.vns@gmail.com>
2020-10-22 10:58:28 -0300

    Updates GET, POST and DELETE routes

--- src/routes/seriesRoutes.js
+++ src/routes/seriesRoutes.js
@@ -1,1 +1,13 @@
-//
+const express = require('express')
+const router = express.Router()
+
+const controller = require('../controllers/seriesController')
+
+router.get('/', controller.getAllSeries)
+router.get('/:id', controller.getByID)
+router.post('/', controller.postSerie)
+// router.put('/', controller.putSerie)
+router.delete('/:id', controller.deleteSerie)
+// router.patch('/', controller.patchSerie)
+
+module.exports = router
